fd4adad9f177d87524b55e5b3f1fae0cbb7cc11e
Gael Borchardt <gaelborchardt@gmail.com>
2025-01-20 12:41:14 -0700

    aaaa

--- js/functions.js
+++ js/functions.js
@@ -16,16 +16,32 @@ function showQueen(cell) {
     counter--;
   }
 }
-function clear() {
-  counter = 0;
+function limpiar() {
+  document.querySelectorAll("td").forEach(td=>td.style.backgroundColor="");  
+  
 }
 
 function changeColor(r, c) {
-    var cell= document.getElementById("board");
+    var celda= document.getElementById("board");
     var r1=r, c1=c , r2=r, c2=c;
     var r3 = r, c3=c, r4=r,c4=c;
-    for (let i = 0; i < array.length; i++) {
-        const element = array[index];
-        
-    }
+    for (let i = 0; i < 8; i++) {
+        celda.rows[r].cells[i].style.backgroundColor = "red";
+        celda.rows[i].cells[c].style.backgroundColor = "red";
+
+
+        if (r1<8 && c1<8){
+          celda.rows[r1++].cells[c1++].style.backgroundColor = "red";
+        }
+        if(r2>-1 && c2<8){
+          celda.rows[r2--].cells[c2++].style.backgroundColor = "red";
+        }
+
+        if(r3>-1 && c3>-1){
+          celda.rows[r3--].cells[c3--].style.backgroundColor = "red";
+        }
+        if(r4<8 && c4>-1){
+          celda.rows[r4++].cells[c4--].style.backgroundColor = "red";
+        }
+  }
 }
